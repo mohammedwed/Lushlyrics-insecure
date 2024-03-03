215d26a89b0b498ab5d8fa1dd422f3d855783d8f
suhelkhanca <suhel.hata@gmail.com>
2024-03-03 07:40:13 +0530

    final touch to the project

--- loginSignup/base/views.py
+++ loginSignup/base/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 @login_required
 def home(request):
-        return render(request, "home.html", {})
+ return render(request, "home.html", {})
+
+
 def authView(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-    else:
-        form = UserCreationForm()
-    return render(request, "registration/signup.html", {"form": form})
+ if request.method == "POST":
+  form = UserCreationForm(request.POST or None)
+  if form.is_valid():
+   form.save()
+   return redirect("base:login")
+ else:
+  form = UserCreationForm()
+ return render(request, "registration/signup.html", {"form": form})
