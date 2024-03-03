f0afdd869fa803ae0f2b7af63b56791f24146056
suhelkhanca <suhel.hata@gmail.com>
2024-03-03 08:48:21 +0530

    changes required

--- loginSignup/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import authView, home
-
-urlpatterns = [
- path("", home, name="home"),
- path("signup/", authView, name="authView"),
- path("accounts/", include("django.contrib.auth.urls")),
-]
